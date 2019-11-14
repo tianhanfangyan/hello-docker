6d439252e8d54fd1ff709ae895ed25995e2c1c27
daiyang <dystargate@gmail.com>
2019-11-14 16:16:33 +0800

    Alter the app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET"])
 def index():
     return "Hello Docker!!!"
-
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8888, debug=False)
